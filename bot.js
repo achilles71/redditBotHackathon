2ede2bfaf22d27f324a951a676ede1a05b38d497
achilles71 <67031258+achilles71@users.noreply.github.com>
2021-02-16 00:32:08 +0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,7 +23,7 @@ const reddit = new snoowrap({
 // Our main function that will call the Reddit api for new stories in a specific subreddit
 // if you aren't familiar with ES6 syntax, the sub will default to /r/aww, the number of stories to 10.
 // The arrows are one way of writing functions in ES6.
-getNewStories = (sub='XRP', num=1) => {
+getNewStories = (sub='CryptoCurrencies', num=1) => {
     reddit.getSubreddit(sub).getHot()
     .then(function(listing) {
         for (var i = 0; i < num ; i++) {
@@ -57,11 +57,8 @@ postNewStory = (post) => {
 
 // This function runs getNewStories in several diffrent subreddits and posts them in our subreddit
 getDevStories = () => {
-  getNewStories('dogecoin', 3);
-getNewStories('SatoshiStreetBets', 3);
+  getNewStories('CryptoNews', 1);
   getNewStories('CryptoCurrency', 1);
-getNewStories('Bitcoin', 3);
-  getNewStories('btc', 3);
 }
 
 
